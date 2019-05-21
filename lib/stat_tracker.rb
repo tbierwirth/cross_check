ceda58ce2a79bf0889c43f92ed4e1407ce2f150e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-21 10:41:12 -0600

    Add method to pull info from CSV file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,6 +5,16 @@ require './lib/game_team_stats'
 require './lib/team_info'
 
 class StatTracker
+  attr_reader :games
 
+  @games = []
+  @teams = []
+  @team_info_path = './data/team_info.csv'
+
+  def self.from_csv
+    CSV.foreach(@team_info_path, headers: true) do |row|
+      @teams << row
+    end
+  end
 
 end
