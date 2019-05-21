9ca1d3e2d533cc567a408017d510cb6265e624e5
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-21 14:44:54 -0600

    Add tests to count size of arrays relative to csv data

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -12,15 +12,27 @@ require 'pry'
 class StatTrackerTest < MiniTest::Test
 
   def setup
-    @stat_tracker = StatTracker.new
+    game_path = './data/game.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
-  def test_it_exists
+  def test_stat_tracker_exists
     assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_add_teams_and_count
-    assert_equal 33, StatTracker.from_csv
+    assert_equal 33, @stat_tracker.teams.count
+    assert_equal 7441, @stat_tracker.games.count
+    assert_equal 14882, @stat_tracker.game_team_stats.count
   end
 
 end
