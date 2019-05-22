bfdefa785d3fc6d00b2f71a00976ac81e24c38ff
Oscar Bellechasse <obellech10@gmail.com>
2019-05-21 19:00:23 -0600

    Add test for lowest total score method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -56,4 +56,8 @@ class StatTrackerTest < MiniTest::Test
   def test_highest_total_score
     assert_equal 15, @stat_tracker.highest_total_score
   end
+
+  def test_lowest_total_score
+    assert_equal 1, @stat_tracker.lowest_total_score
+  end
 end
