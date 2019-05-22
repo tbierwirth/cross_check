53ca8a6a8e4f7e58a18d6da757272e7cf62c4ea3
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 10:40:34 -0600

    Rename methods

--- modules/game_stats.rb
+++ modules/game_stats.rb
@@ -1,14 +1,14 @@
 require 'pry'
 module GameStats
 
-  def home_wins
+  def percentage_home_wins
     total_wins = @games.count do |game|
       game.outcome == ("home win REG") || game.outcome == ("home win OT")
     end
     win_percentage = ((total_wins.to_f / @games.count) * 100.00).round(2)
   end
 
-  def away_wins
+  def percentage_visitor_wins
     total_wins = @games.count do |game|
       game.outcome == ("away win REG") || game.outcome == ("away win OT")
     end
