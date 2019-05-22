1129fcff13e25ca3353d38f05ccc1af09ceeb8cd
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-22 12:08:24 -0600

    Merge methods into new location for game_stats module

--- lib/modules/game_stats.rb
+++ lib/modules/game_stats.rb
@@ -1,4 +1,3 @@
-require 'pry'
 module GameStats
 
   def percentage_home_wins
@@ -20,6 +19,27 @@ module GameStats
       game.away_goals.to_i + game.home_goals.to_i
     end
     (total.to_f / @games.count).round(2)
+  end
+
+  def highest_total_score
+    total_goals = @games.map do |game|
+      game.away_goals.to_i + game.home_goals.to_i
+    end
+    total_goals.max_by { |goals| goals }
+  end
+
+  def lowest_total_score
+    total_goals = @games.map do |game|
+      game.away_goals.to_i + game.home_goals.to_i
+    end
+    total_goals.min_by { |goals| goals }
+  end
+
+  def biggest_blowout
+    goal_differential = @games.map do |game|
+      (game.away_goals.to_i - game.home_goals.to_i).abs
+    end
+    goal_differential.max_by { |goals| goals }
   end
 
 end
