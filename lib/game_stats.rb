df4905c71cbcb840f767c2fac2f1db49f56798e1
Oscar Bellechasse <obellech10@gmail.com>
2019-05-21 19:37:33 -0600

    Create method for biggest blowout

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -19,4 +19,14 @@ module GameStats
       goals
     end
   end
+
+  def biggest_blowout
+    goal_differential = []
+    @games.each do |game|
+      goal_differential << (game.away_goals.to_i - game.home_goals.to_i).abs
+    end
+    goal_differential.max_by do |goals|
+      goals
+    end
+  end
 end
