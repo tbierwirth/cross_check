b01c4c7358352fc751fdbc6be28baeb2232b0647
Oscar Bellechasse <obellech10@gmail.com>
2019-05-21 18:59:49 -0600

    Add method for lowest total score

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -9,4 +9,14 @@ module GameStats
       goals
     end
   end
+
+  def lowest_total_score
+    total_goals = []
+    @games.each do |game|
+      total_goals << game.away_goals.to_i + game.home_goals.to_i
+    end
+    total_goals.min_by do |goals|
+      goals
+    end
+  end
 end
