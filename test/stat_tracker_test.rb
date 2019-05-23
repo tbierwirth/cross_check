d53e7208e3a0b5def973d67e4afaf55f2848853d
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-23 16:50:43 -0600

    Add highest and lowest scoring home team tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -97,8 +97,18 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_can_find_lowest_scoring_visitor
-
     expected = "Sabres"
     assert_equal expected, @stat_tracker.lowest_scoring_visitor
   end
+
+  def test_can_find_lowest_scoring_home_team
+    expected = "Sabres"
+    assert_equal expected, @stat_tracker.lowest_scoring_home_team
+  end
+
+  def test_can_find_highest_scoring_home_team
+    expected = "Golden Knights"
+    assert_equal expected, @stat_tracker.highest_scoring_home_team
+  end
+
 end
