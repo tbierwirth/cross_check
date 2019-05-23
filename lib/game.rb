2f1a2a5d34715d276ec960c5ea5c1855021172c8
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-23 15:27:35 -0600

    Add attr_reader for team_name, away_team_id, and away_goals

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,10 @@ class Game
               :season,
               :away_goals,
               :home_goals,
-              :outcome
+              :outcome,
+              :away_team_id,
+              :team_name
+
 
   def initialize(row)
     @game_id = row["game_id"]
@@ -13,8 +16,8 @@ class Game
     @date_time = row["date_time"]
     @away_team_id = row["away_team_id"]
     @home_team_id = row["home_team_id"]
-    @away_goals = row["away_goals"]
-    @home_goals = row["home_goals"]
+    @away_goals = row["away_goals"].to_i
+    @home_goals = row["home_goals"].to_i
     @outcome = row["outcome"]
     @home_rink_side_start = row["home_rink_side_start"]
     @venue = row["venue"]
