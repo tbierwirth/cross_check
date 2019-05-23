242af75e8cdc4a14600fa851c8a498156d352121
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-23 15:26:16 -0600

    Add methods to calculate best and worst offense

--- lib/modules/league_stats.rb
+++ lib/modules/league_stats.rb
@@ -2,13 +2,41 @@ require "pry"
 module LeagueStats
 
   def best_offense
-    average_goals_per_team = Hash.new(0)
+    total_goals_per_team = Hash.new(0)
     total_games_per_team = Hash.new(0)
-    @game_team_stats.each do |game|
-      average_goals_per_team[game.team_id] +=
-      (game.goals.to_i / total_games_per_team[game.team_id] += 1)
+    @games.each do |game|
+      total_goals_per_team[game.away_team_id] += game.away_goals.to_i
+      total_goals_per_team[game.home_team_id] += game.home_goals.to_i
     end
-    highest_average = average_goals_per_team.max_by{ |team_id, goals| goals}
+    @games.each do |game|
+      total_games_per_team[game.away_team_id] += 1
+      total_games_per_team[game.home_team_id] += 1
+    end
+    average = total_goals_per_team.merge(total_games_per_team) do |team_id, total_goals, games|
+      total_goals / games.to_f
+    end
+    highest_average = average.max_by{ |team_id, goals| goals}
+    team = @teams.find do |team|
+      highest_average[0] == team.team_id
+    end
+    team.team_name
+  end
+
+  def worst_offense
+    total_goals_per_team = Hash.new(0)
+    total_games_per_team = Hash.new(0)
+    @games.each do |game|
+      total_goals_per_team[game.away_team_id] += game.away_goals.to_i
+      total_goals_per_team[game.home_team_id] += game.home_goals.to_i
+    end
+    @games.each do |game|
+      total_games_per_team[game.away_team_id] += 1
+      total_games_per_team[game.home_team_id] += 1
+    end
+    average = total_goals_per_team.merge(total_games_per_team) do |team_id, total_goals, games|
+      total_goals / games.to_f
+    end
+    highest_average = average.min_by{ |team_id, goals| goals}
     team = @teams.find do |team|
       highest_average[0] == team.team_id
     end
