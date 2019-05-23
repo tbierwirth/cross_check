30e58f8bfffdcaf194eeb8294bfe0020139d5a74
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-23 16:32:42 -0600

    Add methods to calculate best and worst defense

--- lib/modules/league_stats.rb
+++ lib/modules/league_stats.rb
@@ -43,4 +43,48 @@ module LeagueStats
     team.team_name
   end
 
+  def best_defense
+    # Lowest goals allowed per game
+    total_goals_allowed = Hash.new(0)
+    total_games_played = Hash.new(0)
+    @games.each do |game|
+      total_goals_allowed[game.away_team_id] += game.home_goals.to_i
+      total_goals_allowed[game.home_team_id] += game.away_goals.to_i
+    end
+    @games.each do |game|
+      total_games_played[game.away_team_id] += 1
+      total_games_played[game.home_team_id] += 1
+    end
+    average = total_goals_allowed.merge(total_games_played) do |team_id, total_goals, games|
+      total_goals / games.to_f
+    end
+    highest_average = average.min_by{ |team_id, goals| goals}
+    team = @teams.find do |team|
+      highest_average[0] == team.team_id
+    end
+    team.team_name
+  end
+
+  def worst_defense
+    # Lowest goals allowed per game
+    total_goals_allowed = Hash.new(0)
+    total_games_played = Hash.new(0)
+    @games.each do |game|
+      total_goals_allowed[game.away_team_id] += game.home_goals.to_i
+      total_goals_allowed[game.home_team_id] += game.away_goals.to_i
+    end
+    @games.each do |game|
+      total_games_played[game.away_team_id] += 1
+      total_games_played[game.home_team_id] += 1
+    end
+    average = total_goals_allowed.merge(total_games_played) do |team_id, total_goals, games|
+      total_goals / games.to_f
+    end
+    highest_average = average.max_by{ |team_id, goals| goals}
+    team = @teams.find do |team|
+      highest_average[0] == team.team_id
+    end
+    team.team_name
+  end
+
 end
