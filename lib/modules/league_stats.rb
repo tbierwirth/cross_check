e4269e60346bb78494aaad747d1cd27b4408b8c4
Oscar Bellechasse <obellech10@gmail.com>
2019-05-22 20:31:18 -0600

    Create method for lowest scoring visitor

--- lib/modules/league_stats.rb
+++ lib/modules/league_stats.rb
@@ -22,4 +22,25 @@ module LeagueStats
     lowest_home_team.team_name
   end
 
+  def lowest_scoring_visitor
+    visitor_goals_by_team = Hash.new(0)
+    visitor_games_by_team = Hash.new(0)
+
+    @game_team_stats.each do |game|
+      if game.hoa == "away"
+        visitor_goals_by_team[game.team_id] += game.goals.to_i
+        visitor_games_by_team[game.team_id] += 1
+      end
+    end
+    visitor_games_by_team.each do |team, games|
+      visitor_goals_by_team[team] = (visitor_goals_by_team[team] / games.to_f).round(2)
+    end
+    lowest_scoring_team = visitor_goals_by_team.min_by {|team, avg_goals| avg_goals}
+    lowest_visitor_team = @teams.find do |team|
+      if team.team_id == lowest_scoring_team[0]
+        team.team_name
+      end
+    end
+    lowest_visitor_team.team_name
+  end
 end
