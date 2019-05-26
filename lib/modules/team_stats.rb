bdb520821ebddf98dec8fb83bf3dd6ecf755a455
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-26 09:26:27 -0600

    Add method to calculate fewest goals scored

--- lib/modules/team_stats.rb
+++ lib/modules/team_stats.rb
@@ -13,4 +13,16 @@ module TeamStats
     goals.max
   end
 
+  def fewest_goals_scored(id)
+    goals = []
+    @games.select do |game|
+      if game.away_team_id == id
+        goals << game.away_goals
+      elsif game.home_team_id == id
+        goals << game.home_goals
+      end
+    end
+    goals.min
+  end
+
 end
