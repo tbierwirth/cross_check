2a72d6e078fd0ab3fa8dd67d1f2f109ee1def76d
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-26 14:00:08 -0600

    Add method to calculate win percentage of a team against all their opponents

--- lib/modules/team_stats.rb
+++ lib/modules/team_stats.rb
@@ -25,4 +25,45 @@ module TeamStats
     goals.min
   end
 
+  # def transform_keys(hash)
+  # result = {}
+  # hash.each_pair do |key, value|
+  #   result[yield(key)] = value
+  # end
+  # result
+  # end
+
+  def head_to_head(id)
+    wins = Hash.new(0)
+    games = Hash.new(0)
+    @games.each do |game|
+      if game.away_team_id == id
+        games[game.home_team_id] += 1
+        if game.outcome.include?("away win")
+          wins[game.home_team_id] += 1
+        end
+      elsif game.home_team_id == id
+        games[game.away_team_id] += 1
+        if game.outcome.include?("home win")
+          wins[game.away_team_id] += 1
+        end
+      end
+    end
+    percentages = games.merge(wins) do |team_id, games, wins|
+      (wins / games.to_f).round(2)
+    end
+    result = {}
+    percentages.each_pair do |team_id, percentage|
+      result[team_name(team_id)] = percentage
+    end
+    result
+  end
+
+  def team_name(id)
+    team = @teams.find do |team|
+      id == team.team_id
+    end
+    team.team_name
+  end
+
 end
