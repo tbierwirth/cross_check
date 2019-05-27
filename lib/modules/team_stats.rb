19ec7ba5eac42a521c2e65f4a4d3f7b54a5068a5
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-27 00:57:03 -0600

    Add rival method

--- lib/modules/team_stats.rb
+++ lib/modules/team_stats.rb
@@ -53,4 +53,48 @@ module TeamStats
     end
     team.team_name
   end
-end 
+
+  def rival(team_id)
+  #Name of the opponent that has the highest win percentage against the given team.
+    relevant_games = @games.find_all do |game|
+      game.home_team_id == team_id || game.away_team_id == team_id
+    end
+
+    games_played = Hash.new(0)
+    #{opponent_id => times opponent played target_team}
+    relevant_games.map do |game|
+      if team_id == game.home_team_id
+        opponent = game.away_team_id
+        games_played[opponent] += 1
+      else
+        opponent = game.home_team_id
+        games_played[opponent] += 1
+      end
+    end
+
+    wins = Hash.new(0)
+    #{oppenent_id => times that target_team has won against opponent}
+    relevant_games.map do |game|
+      if team_id == game.home_team_id && game.home_goals > game.away_goals
+        #wins[team_id] += 1
+        #commented out because adding up all of team_id's wins moves it to the top of the list. Not what we want. 
+      elsif team_id == game.home_team_id && game.home_goals < game.away_goals
+        wins[game.away_team_id] += 1
+      elsif team_id == game.away_team_id && game.home_goals < game.away_goals
+        #wins[team_id] += 1
+      elsif team_id == game.away_team_id && game.home_goals > game.away_goals
+        wins[game.home_team_id] += 1
+      end
+    end
+    win_percentage = games_played.merge(wins) do |opponent_id, num_of_games, num_won|
+      num_won / num_of_games.to_f
+    end
+    highest_percentage = win_percentage.max_by do |id, percentage|
+      percentage
+    end
+    team = @teams.find do |team|
+      highest_percentage.first == team.team_id
+    end
+    team.team_name
+  end
+end
