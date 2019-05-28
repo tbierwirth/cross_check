ef644da87037a4aaf38e579757a3728285d6515f
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-28 11:05:43 -0600

    Add winningest_coach method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -312,10 +312,15 @@ class StatTrackerTest < MiniTest::Test
     assert_equal 0.52, @stat_tracker.average_win_percentage("18")
   end
 
-  def test_can_return_the_worst_coach_of_a_season
+  def test_can_return_the_worst_coach_of_season
 
     assert_equal "Peter Laviolette", @stat_tracker.worst_coach("20132014")
     assert_equal "Craig MacTavish", @stat_tracker.worst_coach("20142015")
   end
 
+  def test_can_return_the_best_coach_of_season
+
+  assert_equal "Claude Julien", @stat_tracker.winningest_coach("20132014")
+  assert_equal "Alain Vigneault", @stat_tracker.winningest_coach("20142015")
+  end
 end
