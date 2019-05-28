a6ded42a66dccdb11405a02205f5dc01055e0764
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-28 11:05:06 -0600

    Add test for winningest_coach

--- lib/modules/season_statistics.rb
+++ lib/modules/season_statistics.rb
@@ -9,24 +9,54 @@ module SeasonStatistics
       end
     end
     games_played = Hash.new(0)
-    games_won = Hash.new(0)
+    games_lost = Hash.new(0)
 
     @game_team_stats.find_all do |game|
       relevant_game_id.find_all do |id|
          if id == game.game_id
               games_played[game.head_coach] += 1
-            elsif id == game.game_id && game.won == "TRUE"
-            games_won[game.head_coach] += 1
-
+            if id == game.game_id && game.won == "FALSE"
+            games_lost[game.head_coach] += 1
+            end
           end
         end
       end
-    win_percentage = games_won.merge(games_played) do |coach, won, played|
-      won / played.to_f
+    win_percentage = games_lost.merge(games_played) do |coach, lost, played|
+      1 - (lost / played.to_f)
     end
     worst_percentage = win_percentage.min_by do |coach, percentage|
       percentage
     end
     worst_percentage.first
   end
+
+  def winningest_coach(season)
+    #Name of the Coach with the best win percentage for the season.
+    relevant_game_id = []
+    @games.each do |game|
+      if season == game.season
+        relevant_game_id << game.game_id
+      end
+    end
+    games_played = Hash.new(0)
+    games_lost = Hash.new(0)
+
+    @game_team_stats.find_all do |game|
+      relevant_game_id.find_all do |id|
+         if id == game.game_id
+              games_played[game.head_coach] += 1
+            if id == game.game_id && game.won == "FALSE"
+            games_lost[game.head_coach] += 1
+            end
+          end
+        end
+      end
+    win_percentage = games_lost.merge(games_played) do |coach, lost, played|
+      1 - (lost / played.to_f)
+    end
+    best_percentage = win_percentage.max_by do |coach, percentage|
+      percentage
+    end
+    best_percentage.first
+  end 
 end
