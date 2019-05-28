74972e2484d9ae3510d7be43d2da63834b9d2217
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-28 17:56:02 -0600

    Add total_away_goals/games_by_team_id helper methods

--- lib/modules/league_stats.rb
+++ lib/modules/league_stats.rb
@@ -186,49 +186,39 @@ module LeagueStats
   end
 
   def highest_scoring_visitor
-    total_away_goals_by_team_id = Hash.new(0)
-      @games.each do |game|
-        total_away_goals_by_team_id[game.away_team_id] += game.away_goals
-    end
-
-    total_away_games_by_team_id = Hash.new(0)
-      @games.each do |game|
-        total_away_games_by_team_id[game.away_team_id] += 1
-    end
-
     average = total_away_goals_by_team_id.merge(total_away_games_by_team_id)  do |away_team, away_goals, away_games|
       away_goals / away_games.to_f
     end
-
     highest_average = average.max_by{|team_id, goals| goals}
 
-    team = @teams.find do |team|
-      highest_average.first == team.team_id
-    end
+    team = @teams.find{|team| highest_average.first == team.team_id}
     team.team_name
   end
 
   def lowest_scoring_visitor
+    average = total_away_goals_by_team_id.merge(total_away_games_by_team_id) do |away_team, away_goals, away_games|
+      away_goals / away_games.to_f
+    end
+    lowest_average = average.min_by{|team_id, goals| goals}
+
+    team = @teams.find{|team| lowest_average.first == team.team_id}
+    team.team_name
+  end
+
+  def total_away_goals_by_team_id
     total_away_goals_by_team_id = Hash.new(0)
       @games.each do |game|
         total_away_goals_by_team_id[game.away_team_id] += game.away_goals
+      end
+      total_away_goals_by_team_id
     end
 
+  def total_away_games_by_team_id
     total_away_games_by_team_id = Hash.new(0)
       @games.each do |game|
         total_away_games_by_team_id[game.away_team_id] += 1
     end
-
-    average = total_away_goals_by_team_id.merge(total_away_games_by_team_id) do |away_team, away_goals, away_games|
-      away_goals / away_games.to_f
-    end
-
-    lowest_average = average.min_by{|team_id, goals| goals}
-
-    team = @teams.find do |team|
-      lowest_average.first == team.team_id
-    end
-    team.team_name
+    total_away_games_by_team_id
   end
 
   def lowest_scoring_home_team
