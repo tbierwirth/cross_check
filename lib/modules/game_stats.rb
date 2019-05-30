a990415bdd40a4c1cbe4c09464440760cfce5863
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 18:06:28 -0600

    Add helper method total_goals_for_all_games

--- lib/modules/game_stats.rb
+++ lib/modules/game_stats.rb
@@ -22,17 +22,17 @@ module GameStats
   end
 
   def highest_total_score
-    total_goals = @games.map do |game|
-      game.away_goals.to_i + game.home_goals.to_i
-    end
-    total_goals.max_by { |goals| goals }
+    total_goals_for_all_games.max_by { |goals| goals }
   end
 
   def lowest_total_score
-    total_goals = @games.map do |game|
+    total_goals_for_all_games.min_by { |goals| goals }
+  end
+
+  def total_goals_for_all_games
+    @games.map do |game|
       game.away_goals.to_i + game.home_goals.to_i
     end
-    total_goals.min_by { |goals| goals }
   end
 
   def biggest_blowout
@@ -44,11 +44,11 @@ module GameStats
 
   def count_of_games_by_season
     games_by_season = Hash.new(0)
-      games.each do |game|
-       games_by_season[game.season] += 1
-      end
-      games_by_season
-   end
+    games.each do |game|
+      games_by_season[game.season] += 1
+    end
+        games_by_season
+  end
 
   def average_goals_by_season
     goals_by_season = Hash.new(0)
